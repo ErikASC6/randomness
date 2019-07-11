9c6ba29fc6147515f1d4d42dc8ee4951e88f7023
ErikASC6 <esoriano.2016@khalilgibranhs.org>
2019-07-11 11:59:32 -0400

    FunctionsProject

---
+++ randomness.js
@@ -0,0 +1,29 @@
+// GLOBAL VARIABLES //
+/////////////////////
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const foods = [
+"pizza",
+"burgers",
+"rice and beans",
+"lasagna",
+"macaroni and cheese",
+"baked potatoes"
+];
+const students = [
+"Bob",
+"Martin",
+"Danny",
+"Madhi",
+"Harry",
+"Sean",
+"Robert"
+];
+
+//////////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////////
+
+const randomNumber = Math.random();
+console.log(randomNumber * 21);
+const times21 = randomNumber * 10;
+console.log(times21);
